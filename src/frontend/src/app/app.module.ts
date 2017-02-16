7760c9f3ef83f990b0fdae3edd571b63164ee902
Magnus Grindal Bakken <magnusbakken@gmail.com>
2017-02-16 22:40:59 +0100

    added boxscore component

--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BoxscoreComponent } from './boxscore/boxscore.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoxscoreComponent
   ],
   imports: [
     BrowserModule,
