ca41f869ee0c8842ff1efe370386575719f0e16c
Anders Bakken <agbakken@gmail.com>
2018-05-15 23:58:24 -0700

    foo

--- bin/create-timelapse.sh
+++ bin/create-timelapse.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../src/www/garden
 rm -f timelapse.tmp.mp4
-if ! ffmpeg -r 24 -pattern_type glob -i '*.jpg' -s hd1080 -vcodec libx264 timelapse.tmp.mp4 $>/dev/null; then
+if ! ffmpeg -r 24 -pattern_type glob -i '*.jpg' -s hd1080 -vcodec libx264 timelapse.tmp.mp4 &>/dev/null; then
     rm -f timelapse.tmp.mp4
 else
     mv timelapse.tmp.mp4 timelapse.mp4
