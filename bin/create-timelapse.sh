263e1b665638ac4d2f6ae013cb129491387a51d2
Anders Bakken <agbakken@gmail.com>
2018-05-18 20:19:48 -0700

    Balls

--- bin/create-timelapse.sh
+++ bin/create-timelapse.sh
@@ -2,10 +2,21 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../src/www/garden
-rm -f timelapse.tmp.mp4
-if ! ffmpeg -r 24 -pattern_type glob -i '*.jpg' -s hd1080 -vcodec libx264 timelapse.tmp.mp4 &>/dev/null; then
-    rm -f timelapse.tmp.mp4
-else
-    mv timelapse.tmp.mp4 timelapse.mp4
+rm -rf tmp
+mkdir tmp
+cd tmp
+echo 1 > idx
+find .. -name "*.jpg" -maxdepth 1 | sort | while read i; do
+    IDX=`cat idx`
+    # echo $IDX
+    ln -s "$i" "$(printf %05d ${IDX}).jpg"
+    IDX=$(($IDX + 1))
+    echo $IDX > idx
+done
+
+if ffmpeg -r 24 -i '%05d.jpg' -s hd1080 -crf 24 -preset superfast -vcodec libx264 timelapse.mp4; then
+    /bin/mv timelapse.mp4 ../timelapse.mp4
 fi
+
+rm -rf "$PWD"
 
